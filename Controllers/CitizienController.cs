2477f95efeeff5d62b6ab530ad8434b0b619380f
RicsonSampaio <ricsonsampaio@gmail.com>
2021-02-13 20:06:36 -0300

    adding-citizien-model

---
+++ Controllers/CitizienController.cs
@@ -0,0 +1,26 @@
+﻿using AspDotNetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspDotNetMVC.Controllers
+{
+    public class CitizienController : Controller
+    {
+        // GET: Citizien
+        public ActionResult Index()
+        {
+
+            var citizien = new Citizien() 
+            { 
+                Id = 1,
+                Name = "Jotun",
+                rank = 7
+            };
+
+            return View(citizien);
+        }
+    }
+}
---
+++ Models/Citizien.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspDotNetMVC.Models
+{
+    public class Citizien
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int rank { get; set; }
+    }
+}
